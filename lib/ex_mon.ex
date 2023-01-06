d4a38853f313068132b74b9fe5ab9dfe402e642b
Karol-Soares <karolina.soares@sumup.com>
2023-01-06 16:33:08 -0300

    [Study-03] Create Agent to identify the status of round

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,6 +1,39 @@
 defmodule ExMon do
-  alias ExMon.Player
+
+  @moduledoc """
+  Documentation for ExMon
+
+  It's a game based on turns, where we have one player and the computer.
+  - Human and computer begin with 100 life points
+  - Each round, each player will be able to make 3 moves: moderate move, random movie and heal move
+
+  With each move, what happened and the situation of each player must be showed on the screen. *****
+
+  Examples
+
+  iex> ExMon.create_player("Karol", :chute, :soco, :cura)
+  %ExMon.Player{
+    life: 100,
+    move_avg: :chute,
+    move_heal: :cura,
+    move_rnd: :soco,
+    name: "Karol"
+  }
+
+  """
+  alias ExMon.{Game, Player}
+  alias ExMon.Game.Status
+
+  @computer_name "BenderRobot"
   def create_player(name, move_avg, move_heal, move_rnd) do
     Player.build(name, move_rnd, move_avg, move_heal)
+  end
+
+  def start_game(player) do
+    @computer_name
+    |> create_player(:punch, :kick, :heal)
+    |> Game.start(player)
+
+    Status.print_round_message()
   end
 end
