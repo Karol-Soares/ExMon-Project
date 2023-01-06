9364f996c19843a385dec97b68ee164b5ec3c9de
Karol-Soares <karolina.soares@sumup.com>
2023-01-06 16:54:22 -0300

    [Study-03] Fix spaces

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -1,7 +1,7 @@
 defmodule ExMon.Game.Status do
   alias ExMon.Game
   def print_round_message() do
-    IO.puts("\n=====The game is started! =====\n")
+    IO.puts("\n=====   The game is started!  =====\n")
     IO.inspect(Game.info())
     IO.puts("------------------------------------------")
   end
