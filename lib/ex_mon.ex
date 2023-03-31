406626b923073fd261437d9e5f624f6aa9691be2
Karol-Soares <karolina.soares@sumup.com>
2023-03-31 16:44:09 -0300

    [Study-04] Veryfing player move

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -21,7 +21,7 @@ defmodule ExMon do
 
   """
   alias ExMon.{Game, Player}
-  alias ExMon.Game.Status
+  alias ExMon.Game.{Actions, Status}
 
   @computer_name "BenderRobot"
   def create_player(name, move_avg, move_heal, move_rnd) do
@@ -34,5 +34,9 @@ defmodule ExMon do
     |> Game.start(player)
 
     Status.print_round_message()
+  end
+
+  def make_move(move) do
+    Actions.fetch_move(move)
   end
 end
